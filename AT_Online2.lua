258300c7bf6de97a1c16266d61b5bb157effc5c5
PanSeek <35432222+PanSeek@users.noreply.github.com>
2022-07-04 17:48:40 +0300

    Add files via upload

---
+++ AT_Online2.lua
@@ -0,0 +1,28 @@
+local t = {
+    afk = 0,
+    noAFK = 0,
+    online = 0
+}
+
+function main()
+	local startTime = os.time()
+    while true do wait(1000)
+        if sampGetGamestate() == 3 then
+            t.afk = (os.time() - startTime) - t.noAFK
+            t.noAFK = t.noAFK + 1
+            t.online = t.noAFK + t.afk
+        end
+    end
+end
+
+function EXPORTS.get_times()
+    return t
+end
+
+function onScriptTerminate(script, quitGame)
+    for _, it in pairs(script) do
+        if script.filename == 'AdminTools.lua' then
+            thisScript():reload()
+        end
+    end
+end
